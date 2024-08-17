d5e69f193f4d77a99641d457afe40ed9a3b5aea5
shanakasp <74164029+shanakasp@users.noreply.github.com>
2024-08-17 09:48:25 +0530

    modified 06stringMethod.js in .

--- 06stringMethod.js
+++ 06stringMethod.js
@@ -1,9 +1,11 @@
 let text = "javascript is good";
 
 let CapitalText = text.toUpperCase();
-let CapitalFirstLetter = text.charAt(0).toUpperCase();
+
 let SimpleText = text.toLowerCase();
+let CapitalFirstLetter =
+  text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
 
 console.log(
-  `Capital Text : ${CapitalText} , Simple Text : ${SimpleText} , Capital First Letter : ${CapitalFirstLetter} ${text}`
+  `Capital Text : ${CapitalText} , Simple Text : ${SimpleText} , Capital First Letter : ${CapitalFirstLetter}`
 );
