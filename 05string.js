1ba06ad242745e1710f9cc3fe511cfb4f33e80cb
shanakasp <74164029+shanakasp@users.noreply.github.com>
2024-08-17 09:47:06 +0530

    changed 3 files

--- 05string.js
+++ 05string.js
@@ -4,3 +4,4 @@ let lastname = "prince";
 let fullname = firstname + " " + lastname;
 
 console.log(fullname);
+console.log("Length of the full name: ", fullname.length);
---
+++ 06stringMethod.js
@@ -0,0 +1,9 @@
+let text = "javascript is good";
+
+let CapitalText = text.toUpperCase();
+let CapitalFirstLetter = text.charAt(0).toUpperCase();
+let SimpleText = text.toLowerCase();
+
+console.log(
+  `Capital Text : ${CapitalText} , Simple Text : ${SimpleText} , Capital First Letter : ${CapitalFirstLetter} ${text}`
+);
