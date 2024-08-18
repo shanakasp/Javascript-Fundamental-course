27c181b8908d4c5e0a23a79f0d37de6a7438a4b9
shanakasp <74164029+shanakasp@users.noreply.github.com>
2024-08-18 10:07:01 +0530

    modified index.html added 07stringSearch.js

---
+++ 07stringSearch.js
@@ -0,0 +1,7 @@
+let sentence = "Javascript is good";
+let indexof = sentence.indexOf("is");
+console.log(indexof);
+
+let regex = /javascript/i;
+let regexResult = sentence.match(regex);
+console.log(regexResult);
