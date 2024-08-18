656930ea121610878bd699ba337f0c19fb6a0bc4
shanakasp <74164029+shanakasp@users.noreply.github.com>
2024-08-18 10:44:11 +0530

    modified index.html added 10boolean.js

---
+++ 10boolean.js
@@ -0,0 +1,12 @@
+const age = 25;
+const isAdult = age >= 10;
+
+const temp = 25;
+if (temp >= 25) {
+  console.log("It is a hot day");
+} else if (temp >= 15) {
+  console.log("It is a pleasant day");
+} else {
+  console.log("It is a cold day");
+}
+console.log(isAdult);
