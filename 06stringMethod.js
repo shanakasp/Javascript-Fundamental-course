cee192d39d81b84ff75248f0411f923c89a9a8ce
shanakasp <74164029+shanakasp@users.noreply.github.com>
2024-08-18 09:55:44 +0530

    modified 06stringMethod.js in .

--- 06stringMethod.js
+++ 06stringMethod.js
@@ -6,6 +6,12 @@ let SimpleText = text.toLowerCase();
 let CapitalFirstLetter =
   text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
 
+let song = " I will go school";
+
+let newSong = song.replace("school", "class");
+
+console.log(newSong);
+
 console.log(
   `Capital Text : ${CapitalText} , Simple Text : ${SimpleText} , Capital First Letter : ${CapitalFirstLetter}`
 );
